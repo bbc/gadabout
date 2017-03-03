b791f11f76176a4eee438baf6cd0351cfd2f4992
Ben Clark <ben.clark02@bbc.co.uk>
2017-03-03 10:09:00 +0000

    Build in docker

---
+++ build/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o nounset -o errexit -o pipefail -o errtrace
+
+mkdir -p ~/.gem
+
+echo -e "---\n:rubygems_api_key: 'missing'" > ~/.gem/credentials
+
+curl -u${ARTIFACTORY_USERNAME}:${ARTIFACTORY_API_KEY}                                                             \
+	     https://artifactory-noforge.virt.ch.bbc.co.uk/artifactory/api/gems/irfs-rubygems/api/v1/api_key.yaml \
+	          | grep rubygems_api_key | sed 's/rubygems_api_key/artifactory_api_key/g' >> ~/.gem/credentials
+
+chmod 0600 ~/.gem/credentials
+
+/usr/local/bin/gem build ./gadabout.gemspec
+/usr/local/bin/gem push bbc-gadabout-0.5.8.gem \
+    --key artifactory_api_key \
+    --host https://artifactory-noforge.virt.ch.bbc.co.uk/artifactory/api/gems/irfs-rubygems
